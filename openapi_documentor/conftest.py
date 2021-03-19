a21ea9ac10e909f4ec7ddf356d19a9e6ee852957
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2021-03-19 19:26:04 +0530

    initital commit

---
+++ openapi_documentor/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from openapi_documentor.users.models import User
+from openapi_documentor.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
