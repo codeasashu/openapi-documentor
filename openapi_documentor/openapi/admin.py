aec829a2db47171b6060fb82a7512cee9d8a6eb2
Ashutosh Chaudhary <216.ashutosh@gmail.com>
2021-03-22 23:50:48 +0530

    precommit fixes

--- openapi_documentor/openapi/admin.py
+++ openapi_documentor/openapi/admin.py
@@ -3,15 +3,17 @@ from django.contrib import admin
 # Register your models here.
 # from guardian.admin import GuardedModelAdmin
 from .models import Document
+
 # admin.site.register(Revision)
+
 
 @admin.register(Document)
 class DocumentAdmin(admin.ModelAdmin):
     prepopulated_fields = {"title": ("title",)}
-    list_display = ('title', 'created')
-    search_fields = ('title',)
-    ordering = ('-created',)
-    date_hierarchy = 'created'
+    list_display = ("title", "created")
+    search_fields = ("title",)
+    ordering = ("-created",)
+    date_hierarchy = "created"
 
     def get_queryset(self, request):
         qs = super(DocumentAdmin, self).get_queryset(request)
--- openapi_documentor/openapi/views.py
+++ openapi_documentor/openapi/views.py
@@ -1,33 +1,40 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
+from django.views.generic import DetailView, ListView
 from taggit.models import Tag
+
 from .models import Document
+
 
 class OpenapiListView(LoginRequiredMixin, ListView):
     model = Document
-    context_object_name = 'apis'
+    context_object_name = "apis"
     paginate_by = 10
+
 
 api_list_view = OpenapiListView.as_view()
 
+
 class OpenapiDetailView(LoginRequiredMixin, DetailView):
     model = Document
-    context_object_name = 'api'
+    context_object_name = "api"
+
 
 api_detail_view = OpenapiDetailView.as_view()
 
+
 class OpenapiTaggedView(LoginRequiredMixin, ListView):
-    context_object_name = 'apis'
+    context_object_name = "apis"
     paginate_by = 10
-    template_name = 'document_list.html'
+    template_name = "document_list.html"
 
     def get_queryset(self):
-        slug = self.kwargs.get('tag', None)
+        slug = self.kwargs.get("tag", None)
         if slug:
             tag = get_object_or_404(Tag, slug=slug)
             return Document.objects.filter(tags=tag)
         else:
             return Document.objects.none()
+
 
 api_tagged_view = OpenapiTaggedView.as_view()
